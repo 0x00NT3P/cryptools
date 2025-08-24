feb66ad700cc15c509392142d3159b41d96b63e2
NT3P <victornisaelrodriguez@gmail.com>
2025-08-24 15:44:56 -0500

    feat(cryptools.py): add mode CBC, ECB, OFB and md5 method

--- cryptools.py
+++ cryptools.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import click
+import click, os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 
 @click.group()
 def cryptools():
@@ -10,25 +10,26 @@ def cryptools():
 
 @cryptools.command()
 @click.option('--key', required=True, help="key value")
-@click.option('--iv', required=True, help="iv value")
 @click.option('--message', required=True, help="text to encrypt")
 @click.pass_context
-def encrypt(ctx, key, iv, message):
-
+def encrypt(ctx, key, message):
     messageBytes = message.encode('utf-8')
-    ivBytes = iv.encode('utf-8')
     keyBytes = key.encode('utf-8')
+    ivBytes = os.urandom(16)
 
-    cipher = Cipher(algorithms.AES(keyBytes), modes.CBC(ivBytes))
+    #cipher = Cipher(algorithms.AES(keyBytes), modes.CBC(ivBytes))
+    # cipher = Cipher(algorithms.AES(keyBytes), modes.ECB())
+    cipher = Cipher(algorithms.AES(keyBytes), modes.OFB(ivBytes))
 
     # Agregar padding
-    padder = padding.PKCS7(128).padder()
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
     padded_data = padder.update(messageBytes) + padder.finalize()
 
     encryptor = cipher.encryptor()
     ct = encryptor.update(padded_data) + encryptor.finalize()
 
     hex_text = ct.hex().upper()
+    print(f"IV : {ivBytes.hex().upper()}")
     print(f"Encrypted (hex): {hex_text}")
 
 @cryptools.command()
@@ -37,16 +38,13 @@ def encrypt(ctx, key, iv, message):
 @click.option('--cryptogram', required=True, help="cryptogram to decrypt")
 @click.pass_context
 def decrypt(ctx, key, iv, cryptogram):
-    # Clave (32 bytes → AES-256)
-    keyBytes = key.encode('utf-8')
-
-    # IV fijo de 16 bytes (¡para pruebas!)
-    ivBytes = iv.encode('utf-8')
-
     ciphertext = bytes.fromhex(cryptogram)
+    keyBytes = key.encode('utf-8')
+    ivBytes = bytes.fromhex(iv)
 
     # Crear cifrador AES CBC
-    cipher = Cipher(algorithms.AES(keyBytes), modes.CBC(ivBytes), backend=default_backend())
+    #cipher = Cipher(algorithms.AES(keyBytes), modes.CBC(ivBytes))
+    cipher = Cipher(algorithms.AES(keyBytes), modes.ECB())
     decryptor = cipher.decryptor()
 
     # Descifrar y quitar padding
@@ -58,6 +56,17 @@ def decrypt(ctx, key, iv, cryptogram):
 
     print("Decrypted:", plaintext.decode())
 
+@cryptools.command()
+@click.option('--file', required=True, help="file path")
+@click.pass_context
+def md5(ctx, file):
+    f = open(file, 'rb+')
+    data = f.read()
+
+    digest = hashes.Hash(hashes.MD5())
+    digest.update(data)
+    ct = digest.finalize()
+    print(f"MD5 (hex): {ct.hex()}")
 
 if __name__ == '__main__':
     cryptools()
